062553e6769aaca9d6cb8cd1f1b6fb87d9ecea0d
hi-dhl <hihuilong@gmail.com>
2020-06-27 22:49:13 +0800

    Site updated: 2020-06-27 22:49:13

--- js/main.js
+++ js/main.js
@@ -24,11 +24,11 @@
         $('.columns .column-right-shadow').append($('.columns .column-right').children().clone());
     }
 
-    if (typeof moment === 'function') {
-        $('.article-meta time').each(function() {
-            $(this).text(moment($(this).attr('datetime')).fromNow());
-        });
-    }
+    // if (typeof moment === 'function') {
+    //     $('.article-meta time').each(function() {
+    //         $(this).text(moment($(this).attr('datetime')).fromNow());
+    //     });
+    // }
 
     $('.article > .content > table').each(function() {
         if ($(this).width() > $(this).parent().width()) {
